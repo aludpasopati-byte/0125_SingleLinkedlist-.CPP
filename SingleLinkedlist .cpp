0ffe19e5c208f146d1c4b826b3f78a5991fbfd2d
aludpasopati-byte <aludpasopati@gmail.com>
2026-04-23 12:24:12 +0700

    class linked

--- SingleLinkedlist .cpp
+++ SingleLinkedlist .cpp
@@ -9,3 +9,35 @@ class Node
     Node* next;
 };
 
+class linkedList
+{
+    Node *start;
+
+    public:
+    linkedList()
+    {
+        start = NULL;
+    }
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan nomor mahasiswa: ";
+        cin >> nim;
+
+        Node *nodebaru = new Node;
+        nodebaru->noMhs = nim;
+
+        if (start == NULL || nim <= start->noMhs)
+        {
+            if (start != NULL && nim == start->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+
+            nodebaru->next = start;
+            start = nodebaru;
+            return;
+        }
+
+          
