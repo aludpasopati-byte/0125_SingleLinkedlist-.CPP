d175d907a820c2c36bba35daf0bab3c800f82406
aludpasopati-byte <aludpasopati@gmail.com>
2026-04-23 12:28:56 +0700

    int main

--- SingleLinkedlist .cpp
+++ SingleLinkedlist .cpp
@@ -134,3 +134,22 @@ class linkedList
     }
 };
 
+int main()
+{
+    linkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Menambah data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. keluar" << endl;
+
+        cout << "\nMasukkan pilihan (1-5): ";
+        cin >> ch;
+
+      
